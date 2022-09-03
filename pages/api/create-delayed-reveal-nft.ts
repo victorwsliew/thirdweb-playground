ffbdcc9f61f2c3d0287d5a74c2556fda0ba58020
victorliew <victorwsliew@gmail.com>
2022-09-03 16:35:37 +0800

    feat: add create delayed reveal nft api

---
+++ pages/api/create-delayed-reveal-nft.ts
@@ -0,0 +1,41 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import fs from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function CreateDelayedRevealNft(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const goerliSDK = ThirdwebSDK.fromPrivateKey(
+    process.env.GOERLI_WALLET_PRIVATE_KEY as string,
+    "goerli"
+  );
+  
+  const contract = goerliSDK.getNFTDrop(
+    process.env.NFT_DROP_CONTRACT_ADDRESS as string,
+  );
+  // the real NFTs, these will be encrypted until you reveal them
+  const realNFTs = [
+    {
+      name: "Common NFT #1",
+      description: "Common NFT, one of many.",
+      animation_url: fs.readFileSync("./public/1.mp3"),
+      image: fs.readFileSync("./public/1.png"),
+    },
+  ];
+  // A placeholder NFT that people will get immediately in their wallet, and will be converted to the real NFT at reveal time
+  const placeholderNFT = {
+    name: "Hidden NFT",
+    description: "Will be revealed next week!",
+    animation_url: fs.readFileSync("./public/1_placeholder.mp3"),
+    image: fs.readFileSync("./public/test.jpg"),
+  };
+
+  await contract.revealer.createDelayedRevealBatch(
+    placeholderNFT,
+    realNFTs,
+    "password"
+  );
+
+  res.status(200).json({ success: true });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const createEncryptNft = async () => {
+    setIsLoading(true);
+    // Create and encrypt the NFTs
+    const init = async () => {
+      await fetch("/api/create-delayed-reveal-nft", {
+        method: "POST",
+      });
+      return;
+    }
+
+    init().then(() => {
+      setIsLoading(false);
+    })
+  };
+
   return (
     <div>
-      <ConnectWallet accentColor="#f213a4" colorMode="light" />
+      <ConnectWallet accentColor="white" colorMode="light" />
+
+      <hr />
+
+      <button onClick={createEncryptNft} disabled={isLoading}>
+        {isLoading ? 'loading....' : 'Create Delayed Reveal NFTs'}
+      </button>
     </div>
   );
 };
